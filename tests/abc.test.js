180ab87bb7e3609e1cf308222cf27a7c234a373b
Timendus <mail@timendus.com>
2021-07-25 22:40:37 +0200

    Add second test program

--- tests/abc.test.js
+++ tests/abc.test.js
@@ -1,20 +1,20 @@
-#!/usr/bin/env node
-
 const {types, run} = require('./test-helper');
 
 run({
   type: types.BLINDVIP,
   rom: './tests/abc.ch8',
-  cycles: 100,
+  cycles: 30,
   test: (ram, display) => {
-    test('shows ABC on the display', () => {
-      expect(Array.from(display.slice(12*8, 17*8))).toEqual([
-        0, 0, 0, 0b00110011, 0b10001110, 0, 0, 0,
-        0, 0, 0, 0b01001010, 0b01010000, 0, 0, 0,
-        0, 0, 0, 0b01111011, 0b10010000, 0, 0, 0,
-        0, 0, 0, 0b01001010, 0b01010000, 0, 0, 0,
-        0, 0, 0, 0b01001011, 0b10001110, 0, 0, 0,
-      ]);
+    describe('abc.ch8', () => {
+      test('shows ABC on the display', () => {
+        expect(Array.from(display.slice(12*8, 17*8))).toEqual([
+          0, 0, 0, 0b00110011, 0b10001110, 0, 0, 0,
+          0, 0, 0, 0b01001010, 0b01010000, 0, 0, 0,
+          0, 0, 0, 0b01111011, 0b10010000, 0, 0, 0,
+          0, 0, 0, 0b01001010, 0b01010000, 0, 0, 0,
+          0, 0, 0, 0b01001011, 0b10001110, 0, 0, 0,
+        ]);
+      });
     });
   }
 });
---
+++ tests/fibonacci.test.js
@@ -0,0 +1,14 @@
+const {types, run} = require('./test-helper');
+
+run({
+  type: types.BLINDVIP,
+  rom: './tests/fibonacci.ch8',
+  cycles: 60,
+  test: (ram, display) => {
+    describe('fibonacci.ch8', () => {
+      test('calculates the 12th Fibonacci number', () => {
+        expect(ram.[0x400]).toBe(144);
+      });
+    });
+  }
+});
