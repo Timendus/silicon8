b236f4d25e9b6dfcf651bb69b6cfaf5d8cd5bccc
Timendus <mail@timendus.com>
2021-07-28 16:35:54 +0200

    Add tests to confirm load and save behaviour on VIP and SCHIP

---
+++ tests/loadstore.test.js
@@ -0,0 +1,45 @@
+const {types, run} = require('./test-helper');
+
+describe('on VIP', () => {
+  run({
+    type: types.BLINDVIP,
+    rom: './tests/loadstore.ch8',
+    cycles: 15,
+    test: (ram, display) => {
+      describe('loadstore.ch8', () => {
+        test('stores in memory and advances i', () => {
+          expect(Array.from(ram.slice(0x400, 0x404))).toEqual([
+            0xAA, 0x55, 0xAA, 0x55
+          ]);
+        });
+        test('reads from memory and advances i', () => {
+          expect(Array.from(ram.slice(0x404, 0x408))).toEqual([
+            0x60, 0xAA, 0xA4, 0x00
+          ]);
+        });
+      });
+    }
+  });
+});
+
+describe('on SCHIP', () => {
+  run({
+    type: types.SCHIP,
+    rom: './tests/loadstore.ch8',
+    cycles: 15,
+    test: (ram, display) => {
+      describe('loadstore.ch8', () => {
+        test('stores in memory in the same place twice', () => {
+          expect(Array.from(ram.slice(0x400, 0x404))).toEqual([
+            0xAA, 0x55, 0x00, 0x00
+          ]);
+        });
+        test('reads from memory in the same place twice', () => {
+          expect(Array.from(ram.slice(0x404, 0x408))).toEqual([
+            0xA4, 0x00, 0xA4, 0x00
+          ]);
+        });
+      });
+    }
+  });
+});
