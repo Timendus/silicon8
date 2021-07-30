d770eb3124c8bc38472e7a4fb99e3318720f2a8a
Timendus <mail@timendus.com>
2021-07-30 17:42:22 +0200

    Yet another thing that *isn't* broken :P

---
+++ tests/jump0.test.js
@@ -0,0 +1,31 @@
+const {types, run} = require('./test-helper');
+
+describe('on VIP', () => {
+  run({
+    type: types.BLINDVIP,
+    rom: './tests/jump0.ch8',
+    cycles: 10,
+    test: (ram, display) => {
+      describe('jump0.ch8', () => {
+        test('jumps to the eighth entry in the jump table', () => {
+          expect(ram[0x400]).toBe(8);
+        });
+      });
+    }
+  });
+});
+
+describe('on SCHIP', () => {
+  run({
+    type: types.SCHIP,
+    rom: './tests/jump0.ch8',
+    cycles: 10,
+    test: (ram, display) => {
+      describe('jump0.ch8', () => {
+        test('jumps to the second entry in the jump table', () => {
+          expect(ram[0x400]).toBe(2);
+        });
+      });
+    }
+  });
+});
