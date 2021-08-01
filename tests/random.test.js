b70582b10fe3caeb8aaf0b41d433b4b250e645ed
Timendus <mail@timendus.com>
2021-08-01 22:12:32 +0200

    Add the same random noise test, but for hires mode

--- tests/random.test.js
+++ tests/random.test.js
@@ -10,14 +10,34 @@ describe('random.ch8', () => {
     },
     test: (ram, display) => {
       test('shows a dot at 20,20 given randomByte produces 20', () => {
-        for ( x = 0; x < 8; x++ )
-          for ( y = 0; y < 32; y++ )
-            if ( x == 2 && y == 20 ) {
-              expect(display[y * 8 + x]).toBe(0b00001000);
-            } else {
-              expect(display[y * 8 + x]).toBe(0);
-            }
+        expectPixel(display, 20, 20, 64, 32);
       });
     }
   });
 });
+
+describe('random-hires.ch8', () => {
+  run({
+    type: types.SCHIP,
+    rom: './tests/random-hires.ch8',
+    cycles: 5,
+    callbacks: {
+      randomByte: () => 50
+    },
+    test: (ram, display) => {
+      test('shows a dot at 50,50 given randomByte produces 50', () => {
+        expectPixel(display, 50, 50, 128, 64);
+      });
+    }
+  });
+});
+
+function expectPixel(display, xPos, yPos, w, h) {
+  for ( x = 0; x < (w / 8); x++ )
+    for ( y = 0; y < h; y++ )
+      if ( x == Math.floor(xPos / 8) && y == yPos ) {
+        expect(display[y * (w / 8) + x]).toBe(0b10000000 >> xPos % 8);
+      } else {
+        expect(display[y * (w / 8) + x]).toBe(0);
+      }
+}
