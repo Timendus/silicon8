b20ea845df35f865ebc68ae83456f40db6f86f88
Timendus <mail@timendus.com>
2021-08-13 00:12:53 +0200

    Webpack-in a .ch8 file as the first binary to run

--- web-client/index.js
+++ web-client/index.js
@@ -1,6 +1,11 @@
 import Thimbleful from 'thimbleful';
 const types = require('../shared/types');
 
+import welcome from "!!binary-loader!../welcome.ch8";
+const welcomeProgram = new Uint8Array(welcome.length);
+for (let i = 0; i < welcome.length; i++)
+  welcomeProgram[i] = welcome.charCodeAt(i);
+
 window.addEventListener('load', async () => {
   const Emulator = require('./emulator');
   const keyboard = require('./keyboard');
@@ -11,14 +16,7 @@ window.addEventListener('load', async () => {
   await instance.init();
   keyboard(instance);
 
-  const program = new Uint8Array([
-    0x00, 0xe0, 0x61, 0x19, 0x60, 0x0a, 0x22, 0x12, 0x60,
-    0x0b, 0x22, 0x12, 0x60, 0x0c, 0x22, 0x12, 0x12, 0x10,
-    0x62, 0x0c, 0xf0, 0x29, 0xd1, 0x25, 0x71, 0x05, 0x00,
-    0xee
-  ]);
-
-  instance.loadProgram(types.AUTO, program);
+  instance.loadProgram(types.AUTO, welcomeProgram);
 
   const fileTarget = Thimbleful.FileTarget.instance();
   fileTarget.register('#display', (file, data) => {
