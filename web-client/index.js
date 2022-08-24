0b54fe504da15510aaed68f1cb9abff6a7a32d10
Timendus <mail@timendus.com>
2022-08-24 22:31:19 +0200

    Add quick and dirty support for CBF

--- web-client/index.js
+++ web-client/index.js
@@ -1,5 +1,6 @@
 import Thimbleful from 'thimbleful';
 const types = require('../shared/types');
+const cbf = require('chip8-binary-format');
 
 import welcome from "!!binary-loader!./welcome.ch8";
 const welcomeProgram = new Uint8Array(welcome.length);
@@ -25,13 +26,38 @@ window.addEventListener('load', async () => {
 
   const fileTarget = Thimbleful.FileTarget.instance();
   fileTarget.register('#display', (file, data) => {
-    if ( file.name.substr(-4) != '.ch8' )
-      return alert('You can only load *.ch8 CHIP-8 ROMs');
-    data = atob(data.split(',')[1]);
-    const program = new Uint8Array(new ArrayBuffer(data.length));
-    for ( let i = 0; i < data.length; i++ )
-      program[i] = data.charCodeAt(i);
-    instance.stop();
-    showSettings(program);
+    let program;
+    switch ( file.name.substr(-4) ) {
+      case '.ch8':
+        program = base64ToUint8Array(data);
+        instance.stop();
+        showSettings(program);
+        break;
+      case '.c8b':
+        const file = cbf.unpack(base64ToUint8Array(data));
+        program = file.bytecode[0].bytecode;
+        const typeMap = {
+          1: types.VIP,
+          0x2C: types.SCHIP,
+          0x2D: types.SCHIP,
+          0x34: types.XOCHIP
+        };
+        console.log(file);
+        instance.stop();
+        instance.loadProgram(typeMap[file.bytecode[0].platforms[0]], program);
+        instance.setCyclesPerFrame(file.properties.cyclesPerFrame || 30);
+        instance.start();
+        break;
+      default:
+        return alert('You can only load *.ch8 CHIP-8 ROMs');
+    }
   });
 });
+
+function base64ToUint8Array(data) {
+  data = atob(data.split(',')[1]);
+  const file = new Uint8Array(new ArrayBuffer(data.length));
+  for ( let i = 0; i < data.length; i++ )
+    file[i] = data.charCodeAt(i);
+  return file;
+}
