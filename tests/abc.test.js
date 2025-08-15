bd7d9708028878ccf36080afd3756c1d096a7dab
Timendus <mail@timendus.com>
2025-08-15 18:26:27 +0200

    Reformat tests and commit new scrolling tests

--- tests/abc.test.js
+++ tests/abc.test.js
@@ -1,15 +1,15 @@
-const {types, run} = require('./test-helper');
+const { types, run } = require("./test-helper");
 
 run({
   type: types.BLINDVIP,
-  rom: './tests/abc.ch8',
+  rom: "./tests/abc.ch8",
   cycles: 30,
   test: (ram, display) => {
-    describe('abc.ch8', () => {
-      test('shows ABC on the display', () => {
-        // expect(display).toRenderTo('./tests/abc.bmp');
-        expect(display).toLookLike('./tests/abc.bmp');
+    describe("abc.ch8", () => {
+      test("shows ABC on the display", () => {
+        // expect(display).toRenderTo("./tests/abc.bmp");
+        expect(display).toLookLike("./tests/abc.bmp");
       });
     });
-  }
+  },
 });
--- tests/planes.test.js
+++ tests/planes.test.js
@@ -1,15 +1,15 @@
-const {types, run} = require('./test-helper');
+const { types, run } = require("./test-helper");
 
 run({
   type: types.XOCHIP,
-  rom: './tests/planes.ch8',
+  rom: "./tests/planes.ch8",
   cycles: 50,
   test: (ram, display) => {
-    describe('planes.ch8', () => {
-      test('shows sprites on different layers with different sizes', () => {
-        // expect(display).toRenderTo('./tests/planes.bmp');
-        expect(display).toLookLike('./tests/planes.bmp');
+    describe("planes.ch8", () => {
+      test("shows sprites on different layers with different sizes", () => {
+        // expect(display).toRenderTo("./tests/planes.bmp");
+        expect(display).toLookLike("./tests/planes.bmp");
       });
     });
-  }
+  },
 });
